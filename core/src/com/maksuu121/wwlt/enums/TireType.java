9d76f46a7d99e9cc00fd4f2fe021df94ba8cea0d
Masiu121 <maks.kapolka@gmail.com>
2021-09-08 23:26:39 +0200

    Updated TireType

--- core/src/com/maksuu121/wwlt/enums/TireType.java
+++ core/src/com/maksuu121/wwlt/enums/TireType.java
@@ -1,6 +1,25 @@
 package com.maksuu121.wwlt.enums;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.maksuu121.wwlt.WorldWideLogisticsTycoon;
+
 public enum TireType {
-    WINTER,
-    SUMMER,
+    WINTER("Winter tires", WorldWideLogisticsTycoon.winterTire),
+    SUMMER("Summer tires", WorldWideLogisticsTycoon.summertire);
+
+    String name;
+    Texture texture;
+
+    TireType(String name, Texture texture) {
+        this.name = name;
+        this.texture = texture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
 }
