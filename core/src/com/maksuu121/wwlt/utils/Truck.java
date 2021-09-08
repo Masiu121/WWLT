349ce3e96afcf63a4e93dafc2be79e29d14e2be4
Masiu121 <maks.kapolka@gmail.com>
2021-09-08 23:26:21 +0200

    Updated Track

--- core/src/com/maksuu121/wwlt/utils/Truck.java
+++ core/src/com/maksuu121/wwlt/utils/Truck.java
@@ -1,5 +1,6 @@
 package com.maksuu121.wwlt.utils;
 
+import com.maksuu121.wwlt.enums.TireType;
 import com.maksuu121.wwlt.enums.TruckType;
 
 public class Truck extends Vehicle {
@@ -7,10 +8,21 @@ public class Truck extends Vehicle {
     int fuel;
     int fuelMax;
     int horsePower;
-
+    Tire tire;
 
     public Truck(TruckType truckType, Location location) {
         super(truckType.getName(), truckType.getTexture(), location);
+        fuelMax = truckType.getFuelMax();
+        fuel = fuelMax;
+        horsePower = truckType.getHorsePower();
+        tire = new Tire(TireType.SUMMER);
+    }
 
+    public void changeTires(Tire tire) {
+        this.tire = tire;
+    }
+
+    public void refuel() {
+        fuel = fuelMax;
     }
 }
