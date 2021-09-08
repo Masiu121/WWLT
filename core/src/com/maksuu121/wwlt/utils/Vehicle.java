4ec9512d09fa566373bc3c09cf33f858a65825f1
Masiu121 <maks.kapolka@gmail.com>
2021-09-08 23:14:04 +0200

    Added Vehicle

---
+++ core/src/com/maksuu121/wwlt/utils/Vehicle.java
@@ -0,0 +1,28 @@
+package com.maksuu121.wwlt.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.maksuu121.wwlt.enums.ActionType;
+
+public abstract class Vehicle {
+    public String name;
+    Texture texture;
+    int durability = 100;
+    Location location;
+    int mileage = 0;
+    int age = 0;
+    ActionType action = ActionType.NOTHING;
+
+    public Vehicle(String name, Texture texture, Location location) {
+        this.name = name;
+        this.texture = texture;
+        this.location = location;
+    }
+
+    void repair() {
+        durability = 100;
+    }
+
+    void transfer(Location location) {
+        this.location = location;
+    }
+}
