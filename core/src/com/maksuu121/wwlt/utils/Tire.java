0b7e1eee954f7c84d4d213b516aa249b099c126d
Masiu121 <maks.kapolka@gmail.com>
2021-09-08 23:14:22 +0200

    Added Tire

---
+++ core/src/com/maksuu121/wwlt/utils/Tire.java
@@ -0,0 +1,11 @@
+package com.maksuu121.wwlt.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.maksuu121.wwlt.enums.TireType;
+
+public class Tire {
+    String name;
+    Texture texture;
+    int durability;
+    TireType tireType;
+}
