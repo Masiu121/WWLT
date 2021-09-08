8d2bc718c43655d0fb2af129b90b21997eda0990
Masiu121 <maks.kapolka@gmail.com>
2021-09-08 23:13:44 +0200

    Initial Commit

---
+++ desktop/src/com/maksuu121/wwlt/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.maksuu121.wwlt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.maksuu121.wwlt.WorldWideLogisticsTycoon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WorldWideLogisticsTycoon(), config);
+	}
+}
