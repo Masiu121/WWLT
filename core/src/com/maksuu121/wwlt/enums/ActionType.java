b45265f98ecf128f3e5635c1ddba3ce7eb2a30be
Masiu121 <maks.kapolka@gmail.com>
2021-09-08 23:14:44 +0200

    Added ActionType

---
+++ core/src/com/maksuu121/wwlt/enums/ActionType.java
@@ -0,0 +1,11 @@
+package com.maksuu121.wwlt.enums;
+
+public enum ActionType {
+    NOTHING,
+    DRIVING,
+    REFUELING,
+    REPAIRING,
+    FINISHING,
+    LOADING,
+    UNLOADING;
+}
---
+++ core/src/com/maksuu121/wwlt/utils/Location.java
@@ -0,0 +1,6 @@
+package com.maksuu121.wwlt.utils;
+
+public enum Location {
+    POLAND,
+    GERMANY;
+}
