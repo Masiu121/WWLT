062747d3afc6e776e9c071d21079334d7da238a0
Masiu121 <maks.kapolka@gmail.com>
2021-09-11 22:11:40 +0200

    Updated TruckType

--- core/src/com/maksuu121/wwlt/enums/TruckType.java
+++ core/src/com/maksuu121/wwlt/enums/TruckType.java
@@ -1,11 +1,25 @@
 package com.maksuu121.wwlt.enums;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.maksuu121.wwlt.WorldWideLogisticsTycoon;
+import com.maksuu121.wwlt.gui.WWLTTexture;
 
 public enum TruckType {
-    R730("Scania R730", WorldWideLogisticsTycoon.R730txt, 850, 730),
-    R580("Scania R580", WorldWideLogisticsTycoon.R580txt, 500, 580);
+    R730("Scania R730", WWLTTexture.R730, 850, 730),
+    R580("Scania R580", WWLTTexture.R580, 500, 580),
+    R520("Scania R520", WWLTTexture.R580, 500, 520),
+    R500("Scania R500", WWLTTexture.R580, 500, 500),
+
+    R490("Scania R490", WWLTTexture.R580, 500, 580),
+    R480("Scania R480", WWLTTexture.R580, 500, 580),
+    R450("Scania R450", WWLTTexture.R580, 500, 580),
+    R410("Scania R410", WWLTTexture.R580, 500, 580),
+    R370("Scania R370", WWLTTexture.R580, 500, 580),
+
+    R360("Scania R360", WWLTTexture.R580, 500, 580),
+    R340("Scania R340", WWLTTexture.R580, 500, 580),
+    R320("Scania R320", WWLTTexture.R580, 500, 580),
+    R280("Scania R280", WWLTTexture.R580, 500, 580),
+    R250("Scania R250", WWLTTexture.R580, 500, 580);
 
     String name;
     Texture texture;
