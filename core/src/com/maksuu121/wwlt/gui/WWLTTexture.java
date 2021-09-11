ae03a339b0dfea4671675f6d23e11a1ed7ec530b
Masiu121 <maks.kapolka@gmail.com>
2021-09-11 22:11:16 +0200

    Added WWLTTexture

---
+++ core/src/com/maksuu121/wwlt/gui/WWLTTexture.java
@@ -0,0 +1,40 @@
+package com.maksuu121.wwlt.gui;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public interface WWLTTexture {
+    //V8
+    Texture R730 = new Texture("badlogic.jpg");
+    Texture R580 = new Texture("badlogic.jpg");
+    Texture R520 = new Texture("badlogic.jpg");
+    Texture R500 = new Texture("badlogic.jpg");
+
+    //R6
+    Texture R490 = new Texture("badlogic.jpg");
+    Texture R480 = new Texture("badlogic.jpg");
+    Texture R450 = new Texture("badlogic.jpg");
+    Texture R410 = new Texture("badlogic.jpg");
+    Texture R370 = new Texture("badlogic.jpg");
+
+    //R5
+    Texture R360 = new Texture("badlogic.jpg");
+    Texture R340 = new Texture("badlogic.jpg");
+    Texture R320 = new Texture("badlogic.jpg");
+    Texture R280 = new Texture("badlogic.jpg");
+    Texture R250 = new Texture("badlogic.jpg");
+
+    //Tires
+    Texture winterTire = new Texture("badlogic.jpg");
+    Texture summerTire = new Texture("badlogic.jpg");
+
+    //Trailers
+    Texture defaultTrailer = new Texture("badlogic.jpg");
+    Texture containerTrailer = new Texture("badlogic.jpg");
+    Texture tipperTrailer = new Texture("badlogic.jpg");
+    Texture coolerTrailer = new Texture("badlogic.jpg");
+    Texture liquidTrailer = new Texture("badlogic.jpg");
+    Texture lowTrailer = new Texture("badlogic.jpg");
+    Texture chemicalTrailer = new Texture("badlogic.jpg");
+    Texture radioactiveTrailer = new Texture("badlogic.jpg");
+    Texture heavyTrailer = new Texture("badlogic.jpg");
+}
