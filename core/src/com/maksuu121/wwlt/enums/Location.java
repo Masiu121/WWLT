51b958224d5a5c6cd985e87ac44a5670ea7fbe20
Masiu121 <maks.kapolka@gmail.com>
2021-09-14 18:31:21 +0200

    Added flag to Location

--- core/src/com/maksuu121/wwlt/enums/Location.java
+++ core/src/com/maksuu121/wwlt/enums/Location.java
@@ -1,19 +1,23 @@
 package com.maksuu121.wwlt.enums;
 
+import com.maksuu121.wwlt.gui.WWLTTexture;
 import com.maksuu121.wwlt.utils.Coordinates;
+import com.badlogic.gdx.graphics.Texture;
 import java.util.Random;
 
 public enum Location {
-    POLAND(new Coordinates(51.9194, 19.1451)),
-    GERMANY(new Coordinates(51.1657, 10.4515)),
-    FRANCE(new Coordinates(46.2276, 2.2137)),
-    PORTUGAL(new Coordinates(39.3999, 8.2245)),
-    ITALY(new Coordinates(41.8719, 12.5674));
+    POLAND(new Coordinates(51.9194, 19.1451), WWLTTexture.poland),
+    GERMANY(new Coordinates(51.1657, 10.4515), WWLTTexture.germany),
+    FRANCE(new Coordinates(46.2276, 2.2137), WWLTTexture.france),
+    PORTUGAL(new Coordinates(39.3999, 8.2245), WWLTTexture.portugal),
+    ITALY(new Coordinates(41.8719, 12.5674), WWLTTexture.italy);
 
     Coordinates coordinates;
+    Texture flag;
 
-    Location(Coordinates coordinates) {
+    Location(Coordinates coordinates, Texture flag) {
         this.coordinates = coordinates;
+        this.flag = flag;
     }
 
     public static Location getRandom() {
@@ -23,5 +27,9 @@ public enum Location {
 
     public Coordinates getCoordinates() {
         return coordinates;
+    }
+
+    public Texture getFlag() {
+        return flag;
     }
 }
