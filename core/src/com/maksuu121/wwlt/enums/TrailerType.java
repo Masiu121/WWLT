f9d9f59cddc8cc08e264a6449b58d066603da1af
Masiu121 <maks.kapolka@gmail.com>
2021-09-15 18:51:21 +0200

    Added Price to TrailerType

--- core/src/com/maksuu121/wwlt/enums/TrailerType.java
+++ core/src/com/maksuu121/wwlt/enums/TrailerType.java
@@ -5,22 +5,24 @@ import com.maksuu121.wwlt.WorldWideLogisticsTycoon;
 import com.maksuu121.wwlt.gui.WWLTTexture;
 
 public enum TrailerType {
-    DEFAULT("Default trailer", WWLTTexture.defaultTrailer),
-    CONTAINER("Container trailer", WWLTTexture.containerTrailer),
-    TIPPER("Tipper trailer", WWLTTexture.tipperTrailer),
-    COOLER("Cooler trailer", WWLTTexture.coolerTrailer),
-    LIQUID("Liquid trailer", WWLTTexture.liquidTrailer),
-    LOW("Low trailer", WWLTTexture.lowTrailer),
-    CHEMICAL("Chemical trailer", WWLTTexture.chemicalTrailer),
-    RADIOACTIVE("Radioactive trailer", WWLTTexture.radioactiveTrailer),
-    HEAVY("Heavy trailer", WWLTTexture.heavyTrailer);
+    DEFAULT("Default trailer", WWLTTexture.defaultTrailer, 1),
+    CONTAINER("Container trailer", WWLTTexture.containerTrailer, 1),
+    TIPPER("Tipper trailer", WWLTTexture.tipperTrailer, 1),
+    COOLER("Cooler trailer", WWLTTexture.coolerTrailer, 1),
+    LIQUID("Liquid trailer", WWLTTexture.liquidTrailer, 1),
+    LOW("Low trailer", WWLTTexture.lowTrailer, 1),
+    CHEMICAL("Chemical trailer", WWLTTexture.chemicalTrailer, 1),
+    RADIOACTIVE("Radioactive trailer", WWLTTexture.radioactiveTrailer, 1),
+    HEAVY("Heavy trailer", WWLTTexture.heavyTrailer, 1);
 
     String name;
     Texture texture;
+    float price;
 
-    TrailerType(String name, Texture texture) {
+    TrailerType(String name, Texture texture, float price) {
         this.name = name;
         this.texture = texture;
+        this.price = price;
     }
 
     public String getName() {
@@ -29,5 +31,9 @@ public enum TrailerType {
 
     public Texture getTexture() {
         return texture;
+    }
+
+    public float getPrice() {
+        return price;
     }
 }
