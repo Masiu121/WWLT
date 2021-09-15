de17e59fa6a643121d334b6f370ac58976cfd5c2
Masiu121 <maks.kapolka@gmail.com>
2021-09-15 18:49:43 +0200

    Added Headquarters

---
+++ core/src/com/maksuu121/wwlt/utils/buildings/Headquarters.java
@@ -0,0 +1,15 @@
+package com.maksuu121.wwlt.utils.buildings;
+
+import com.maksuu121.wwlt.enums.Location;
+
+public class Headquarters {
+    Location location;
+
+    public Headquarters(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+}
