7645ad0cb38b140ed426c23ffad8d0a270565654
Masiu121 <maks.kapolka@gmail.com>
2021-09-16 20:22:08 +0200

    Updated Garage

--- core/src/com/maksuu121/wwlt/utils/buildings/Garage.java
+++ core/src/com/maksuu121/wwlt/utils/buildings/Garage.java
@@ -1,5 +1,8 @@
 package com.maksuu121.wwlt.utils.buildings;
 
+import com.maksuu121.wwlt.WorldWideLogisticsTycoon;
+import com.maksuu121.wwlt.enums.TrailerType;
+import com.maksuu121.wwlt.enums.TruckType;
 import com.maksuu121.wwlt.utils.vehicles.Trailer;
 import com.maksuu121.wwlt.utils.vehicles.Truck;
 
@@ -7,19 +10,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Garage {
-    public List<Truck> trucks;
-    public List<Trailer> trailers;
+    List<Truck> trucks;
+    List<Trailer> trailers;
+    int maxTrailers;
+    int maxTrucks;
+    int level;
 
     public Garage() {
         trucks = new ArrayList<>();
         trailers = new ArrayList<>();
     }
 
-    public void addTruck(Truck truck) {
-        trucks.add(truck);
+    private void setGarageSize() {
+        switch(level) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
     }
 
-    public void addTrailer(Trailer trailer) {
-        trailers.add(trailer);
+    public boolean buyTruck(TruckType truckType, Headquarters headquarters, Garage garage) {
+        if(truckType.getPrice() <= WorldWideLogisticsTycoon.balance && trucks.size() < maxTrucks) {
+            Truck truck = new Truck(truckType, headquarters.getLocation());
+            trucks.add(truck);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean buyTrailer(TrailerType trailerType, Headquarters headquarters, Garage garage) {
+        if(trailerType.getPrice() <= WorldWideLogisticsTycoon.balance && trucks.size() < maxTrailers) {
+            Trailer trailer = new Trailer(trailerType, headquarters.getLocation());
+            trailers.add(trailer);
+            return true;
+        }
+        return false;
     }
 }
