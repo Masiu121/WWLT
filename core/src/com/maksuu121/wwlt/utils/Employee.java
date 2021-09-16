c0446b153aaba85b9a9a1a45bae3e7dae376b6a3
Masiu121 <maks.kapolka@gmail.com>
2021-09-16 20:22:32 +0200

    Updated Employee

--- core/src/com/maksuu121/wwlt/utils/Employee.java
+++ core/src/com/maksuu121/wwlt/utils/Employee.java
@@ -1,8 +1,16 @@
 package com.maksuu121.wwlt.utils;
 
 import com.maksuu121.wwlt.enums.ActionType;
+import com.maksuu121.wwlt.enums.EmployeeType;
 
 public class Employee {
     String name;
+    EmployeeType employeeType;
     ActionType actionType;
+
+    public Employee(String name, EmployeeType employeeType) {
+        this.name = name;
+        this.employeeType = employeeType;
+        actionType = ActionType.NOTHING;
+    }
 }
