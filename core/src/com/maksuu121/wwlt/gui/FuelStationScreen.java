735dc0f9aa57c1f7972c2e71556e971281631af3
Masiu121 <maks.kapolka@gmail.com>
2021-09-17 23:46:18 +0200

    Updated FuelStationScreen

--- core/src/com/maksuu121/wwlt/gui/FuelStationScreen.java
+++ core/src/com/maksuu121/wwlt/gui/FuelStationScreen.java
@@ -1,16 +1,5 @@
 package com.maksuu121.wwlt.gui;
 
-import com.maksuu121.wwlt.utils.vehicles.Truck;
-
-import java.sql.Timestamp;
-import java.util.Date;
-
 public class FuelStationScreen {
-    private void refuel(Truck truck) {
-        int litresToRefuel = truck.getFuelMax() - truck.getFuel();
-        int timeToRefuel = litresToRefuel/2;
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
-        truck.refuel();
-    }
 }
