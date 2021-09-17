1da01797e3a8b05a03600777986c550a50ff7ca7
Masiu121 <maks.kapolka@gmail.com>
2021-09-17 23:45:39 +0200

    Added RepairUpdate

---
+++ core/src/com/maksuu121/wwlt/utils/update/RepairUpdate.java
@@ -0,0 +1,14 @@
+package com.maksuu121.wwlt.utils.update;
+
+import com.maksuu121.wwlt.utils.vehicles.Truck;
+import com.maksuu121.wwlt.utils.vehicles.Vehicle;
+
+public class RepairUpdate {
+    public long timestamp;
+    public Vehicle vehicle;
+
+    public RepairUpdate(long timestamp, Truck vehicle) {
+        this.timestamp = timestamp;
+        this.vehicle = vehicle;
+    }
+}
