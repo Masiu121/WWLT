30c105ad0526727bf8e6ce43db3689aad0ced56f
Masiu121 <maks.kapolka@gmail.com>
2021-09-19 13:52:59 +0200

    Updated ErrorCode

---
+++ core/src/com/maksuu121/wwlt/utils/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.maksuu121.wwlt.utils;
+
+public interface ErrorCode {
+    int SUCCES = 1;
+
+    int NOT_ENOUGH_MONEY = -1;
+    int TRUCK_NOT_AVAILABLE = -2;
+    int EMPLOYEE_NOT_AVAILABLE = -3;
+    int NO_REPAIR_CONTRACT = -4;
+    int GARAGE_TOO_SMALL = -5;
+    int NO_TRAILER_SELECTED = -6;
+    int NO_TRUCK_SELECTED = -7;
+    int NO_WAREHOUSE_EMPLOYEE_SELECTED = -8;
+    int NO_DRIVER_EMPLOYEE_SELECTED = -9;
+    int TRAILER_NOT_AVAILABLE = -10;
+}
--- core/src/com/maksuu121/wwlt/utils/ErrorCodes.java
+++
@@ -1,8 +0,0 @@
-package com.maksuu121.wwlt.utils;
-
-public interface ErrorCodes {
-    int NOT_ENOUGH_MONEY = -1;
-    int TRUCK_NOT_AVAILABLE = -2;
-    int EMPLOYEE_NOT_AVAILABLE = -3;
-    int NO_REPAIR_CONTRACT = -4;
-}
