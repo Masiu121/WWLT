5dbcc3530e5a313c340d0314ca8da5de6ff72b82
Masiu121 <maks.kapolka@gmail.com>
2021-09-19 13:52:49 +0200

    Updated Garage

--- core/src/com/maksuu121/wwlt/utils/buildings/Garage.java
+++ core/src/com/maksuu121/wwlt/utils/buildings/Garage.java
@@ -3,13 +3,14 @@ package com.maksuu121.wwlt.utils.buildings;
 import com.maksuu121.wwlt.WorldWideLogisticsTycoon;
 import com.maksuu121.wwlt.enums.TrailerType;
 import com.maksuu121.wwlt.enums.TruckType;
+import com.maksuu121.wwlt.utils.ErrorCode;
 import com.maksuu121.wwlt.utils.vehicles.Trailer;
 import com.maksuu121.wwlt.utils.vehicles.Truck;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Garage {
+public class Garage implements ErrorCode {
     List<Truck> trucks;
     List<Trailer> trailers;
     int maxTrailers;
@@ -24,29 +25,47 @@ public class Garage {
     private void setGarageSize() {
         switch(level) {
             case 1:
+                maxTrailers = 2;
+                maxTrucks = 2;
                 break;
             case 2:
+                maxTrailers = 4;
+                maxTrucks = 4;
                 break;
             case 3:
+                maxTrailers = 8;
+                maxTrucks = 8;
+                break;
+            case 4:
+                maxTrailers = 14;
+                maxTrucks = 14;
                 break;
         }
     }
 
-    public boolean buyTruck(TruckType truckType, Headquarters headquarters, Garage garage) {
-        if(truckType.getPrice() <= WorldWideLogisticsTycoon.balance && trucks.size() < maxTrucks) {
-            Truck truck = new Truck(truckType, headquarters.getLocation());
-            trucks.add(truck);
-            return true;
+    public int buyTruck(TruckType truckType, Headquarters headquarters) {
+        if(truckType.getPrice() <= WorldWideLogisticsTycoon.balance) {
+            if(trucks.size() < maxTrucks) {
+                Truck truck = new Truck(truckType, headquarters.getLocation());
+                trucks.add(truck);
+                return SUCCES;
+            }
+            else
+                return GARAGE_TOO_SMALL;
         }
-        return false;
+        return NOT_ENOUGH_MONEY;
     }
 
-    public boolean buyTrailer(TrailerType trailerType, Headquarters headquarters, Garage garage) {
-        if(trailerType.getPrice() <= WorldWideLogisticsTycoon.balance && trucks.size() < maxTrailers) {
-            Trailer trailer = new Trailer(trailerType, headquarters.getLocation());
-            trailers.add(trailer);
-            return true;
+    public int buyTrailer(TrailerType trailerType, Headquarters headquarters) {
+        if(trailerType.getPrice() <= WorldWideLogisticsTycoon.balance) {
+            if(trucks.size() < maxTrailers) {
+                Trailer trailer = new Trailer(trailerType, headquarters.getLocation());
+                trailers.add(trailer);
+                return SUCCES;
+            }
+            else
+                return GARAGE_TOO_SMALL;
         }
-        return false;
+        return NOT_ENOUGH_MONEY;
     }
 }
